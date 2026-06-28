9279cacd02caf5abf7542d20865593175260f67b
Chapman Pendery <cpendery@vt.edu>
2026-06-28 15:33:17 -0700

    feat: bump formula to 0.0.1-beta.2
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- Formula/shell-use.rb
+++ Formula/shell-use.rb
@@ -1,28 +1,28 @@
 class ShellUse < Formula
   desc "Headless terminal CLI for driving, asserting on, and recording shells"
   homepage "https://github.com/microsoft/shell-use"
-  version "0.0.1-beta.1"
+  version "0.0.1-beta.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/microsoft/shell-use/releases/download/v#{version}/shell-use-aarch64-apple-darwin.tar.gz"
-      sha256 "55aa4fe313c21f54a782247f956d3625882f02684bade03caee98e668673412e"
+      sha256 "01822db7b5883bae44f559e4222d4d9e8695e6ae339cf69ab5f2dcd948423d0e"
     end
     on_intel do
       url "https://github.com/microsoft/shell-use/releases/download/v#{version}/shell-use-x86_64-apple-darwin.tar.gz"
-      sha256 "4a242afcacc3e7de1fd300b9a19d56565efc4d1721f3b41bbf3c83b4c6428b7c"
+      sha256 "27c1f49c71707824a2d16914e45b8029ec59876d4357b33db87788bb0de8ec79"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/microsoft/shell-use/releases/download/v#{version}/shell-use-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "4073470f33836a8f8a29561cfe23628ad7d970f713bb2a783bdb6c0cc6c32bd4"
+      sha256 "82f7fedf8f8cdc1d4bb48319d23318651c516c519fe19d9ab71e1948781447c5"
     end
     on_intel do
       url "https://github.com/microsoft/shell-use/releases/download/v#{version}/shell-use-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "b9e1a571fbfa59f5882b813d2d41db64b7eb236df101ab2e2fabc38a2a7b3f48"
+      sha256 "c40590fb132cf22e4b675a241b16b1ab19fcffd6a76f4be1c2d6c2bec47f1113"
     end
   end
 
